549050946f4f91fe0c47756bc3585930c34033b0
Thomas Braccia <bracciata@gmail.com>
2019-01-19 17:51:08 -0600

    Now removed scrolling from lessonsList

--- src/js/lessonList.js
+++ src/js/lessonList.js
@@ -34,48 +34,53 @@ var listLessons = [{
 ];
 var listOrig = JSON.parse(JSON.stringify(listLessons));
 window.onload = function () {
-   createTheSearchAndSetup();
-   setAllVisible();
+    createTheSearchAndSetup();
+    setAllVisible();
     populate(0);
 };
+
 function createTheSearchAndSetup() {
     var search = document.querySelector(".searchInput");
     search.oninput = function () {
-      determineVisibility();
+        determineVisibility();
     }
-  }
-  function determineVisibility() {
+}
+
+function determineVisibility() {
     setAllVisible();
     determineFilterVisibilty();
     determineSortVisibility();
     recreateSite(list);
-  
-  }
-  function setAllVisible() {
+
+}
+
+function setAllVisible() {
     for (var i = 0; i < listLessons.length; i++) {
         listLessons[i].visible = true;
-      listOrig[i].visible = true;
+        listOrig[i].visible = true;
     }
-  }
-  function determineSortVisibility() {
+}
+
+function determineSortVisibility() {
     //search
     var search = document.querySelector(".searchInput");
     var searchText = search.value;
     if (searchText.length > 0) {
-      for (var i = 0; i < listLessons.length; i++) {
-        if (!listLessons[i].typeOfSite.includes(searchText)) {
-          if (!listLessons[i].jobNumber.includes(searchText)) {
-            if (!listLessons[i].name.includes(searchText)) {
-              if (!listLessons[i].baseSiteURL.includes(searchText)) {
-                listLessons[i].visible = false;
-                listOrig[i].visible = false;
-              }
+        for (var i = 0; i < listLessons.length; i++) {
+            if (!listLessons[i].typeOfSite.includes(searchText)) {
+                if (!listLessons[i].jobNumber.includes(searchText)) {
+                    if (!listLessons[i].name.includes(searchText)) {
+                        if (!listLessons[i].baseSiteURL.includes(searchText)) {
+                            listLessons[i].visible = false;
+                            listOrig[i].visible = false;
+                        }
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
+}
+
 function populate(typeOfList) {
     var areaForItems = document.querySelector(".lessons");
     while (areaForItems.hasChildNodes()) {
@@ -99,21 +104,21 @@ function populate(typeOfList) {
 
 
 function createListLongVideo(video) {
-    return Twig.renderFile('./src/view/videoListItem.twig', {video:video}, (err, html) => {
+    return Twig.renderFile('./src/view/videoListItem.twig', {
+        video: video
+    }, (err, html) => {
         html; // compiled string
-      });
-      
+    });
+
 }
+
 function createCardVideo(video) {
-    return Twig.renderFile('./src/view/videoCard.twig', {video:video}, (err, html) => {
+    return Twig.renderFile('./src/view/videoCard.twig', {
+        video: video
+    }, (err, html) => {
         html; // compiled string
-      });
+    });
 }
-window.onscroll = function () {
-    var titleHeight = document.querySelector(".title").offsetHeight;
-    var pos = titleHeight - window.pageYOffset;
-    console.log(titleHeight);
-    document.querySelector(".nav").style.top = pos + "px";
-};
+
 
 //http://developer.blackberry.com/design/bb10/color.html
